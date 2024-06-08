e3c23cd7d35aef70db0d239376cfed0a7874979f
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-06-08 11:25:10 +0500

    multidimensional arrays in typescript

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Arrays 
+Object.defineProperty(exports, "__esModule", { value: true });
+// let fruit: string[] = ["mango","cherry","banana","watermelon"]
+// console.log(fruit);
+// console.log(fruit[1]);
+// console.log(fruit.length);
+// Multidimensional Arrays 
+let fruits = [
+    ["mango", "apple", "banana"], // index 0
+    ["watermelon", "cherry", "kiwi"], // index 1
+    ["stawberry", "papaya", "peach"] // index 2
+];
+console.log(fruits);
+console.log(fruits[1]);
+console.log(fruits[1][2]);
+console.log(fruits.length);
---
+++ index.ts
@@ -0,0 +1,35 @@
+// Arrays 
+
+import { log } from "console"
+
+// let fruit: string[] = ["mango","cherry","banana","watermelon"]
+// console.log(fruit);
+// console.log(fruit[1]);
+// console.log(fruit.length);
+
+// Multidimensional Arrays 
+
+let fruits: string[][] = [
+    ["mango","apple","banana"],// index 0
+    ["watermelon","cherry","kiwi"],// index 1
+    ["stawberry","papaya","peach"]// index 2
+]
+
+console.log(fruits);
+
+console.log(fruits[1]);
+
+console.log(fruits[1][2]);
+
+console.log(fruits.length);
+
+
+
+
+
+
+
+
+
+
+
